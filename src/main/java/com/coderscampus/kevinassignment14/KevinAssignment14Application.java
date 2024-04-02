4fcb765e683429d84e0a427baa24907aa7386fb5
KevinGallaccio <gallacciokevin@gmail.com>
2024-04-02 19:06:26 +0200

    Initial commit

---
+++ src/main/java/com/coderscampus/kevinassignment14/KevinAssignment14Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus.kevinassignment14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevinAssignment14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevinAssignment14Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/kevinassignment14/KevinAssignment14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.kevinassignment14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevinAssignment14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
